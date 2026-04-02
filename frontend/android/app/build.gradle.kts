18a7c549dc43ec87dec6f04e2f3d2365e3202919
Gerald Emanuel Vargas Morales <202405633@est.umss.edu>
2026-04-02 10:59:42 -0400

    dependencias bien

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.example.lecturas_electricas"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 21  // Mínimo para Bluetooth y Maps
+        minSdk = flutter.minSdkVersion  // Mínimo para Bluetooth y Maps
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
